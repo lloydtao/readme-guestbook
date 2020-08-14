dbf3e0865de9b411108cd6236eb1f0e25fa76a5b
LloydTao <LL23@live.co.uk>
2020-08-14 19:38:17 +0100

    🎉 Create initial action

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+// const sign = require('./sign');
 
 
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    /// Get inputs.
+    core.info('Getting input variables...');
+    const user = core.getInput('user');
+    const message = core.getInput('message');
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
+    /// Sign profile.
+    core.info(`Signing under \"${user}\", with message \"${message}\"...`);
+    // await sign(user, message);
+
+    /// Complete action.
     core.info((new Date()).toTimeString());
-
     core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
+  } 
+  catch (error) {
+    /// Log the error.
     core.setFailed(error.message);
   }
 }
